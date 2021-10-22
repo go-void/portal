5547e48aed1c7719696faa146dea9d1e54968a2d
Techassi <git@techassi.dev>
2021-10-22 20:39:52 +0200

    Update (un)pack util funcs

--- internal/utils/unpack.go
+++ internal/utils/unpack.go
@@ -4,19 +4,19 @@ import "encoding/binary"
 
 // TODO (Techassi): Add offset overflow check
 
-// UnwrapUint8 unwraps a uint8 of 'data' at 'offset'
+// UnpackUint8 unpacks a uint8 of 'data' at 'offset'
 // and returns the new offset
-func UnwrapUint8(data []byte, offset int) (uint8, int) {
+func UnpackUint8(data []byte, offset int) (uint8, int) {
 	return data[offset], offset + 1
 }
 
-// UnpackUint16 unwraps a uint16 of 'data' at 'offset'
+// UnpackUint16 unpacks a uint16 of 'data' at 'offset'
 // and returns the new offset
 func UnpackUint16(data []byte, offset int) (uint16, int) {
 	return binary.BigEndian.Uint16(data[offset:]), offset + 2
 }
 
-// UnpackUint32 unwraps a uint32 of 'data' at 'offset'
+// UnpackUint32 unpacks a uint32 of 'data' at 'offset'
 // and returns the new offset
 func UnpackUint32(data []byte, offset int) (uint32, int) {
 	return binary.BigEndian.Uint32(data[offset:]), offset + 4
