1fa9545f77661f7dfa8d5f5c14b60b17d2d91f21
Techassi <git@techassi.dev>
2021-10-27 21:30:07 +0200

    Add ability to follow domain name pointers

--- internal/wire/unpack.go
+++ internal/wire/unpack.go
@@ -1,6 +1,8 @@
 package wire
 
-import "encoding/binary"
+import (
+	"encoding/binary"
+)
 
 // TODO (Techassi): Add offset overflow check
 
@@ -22,6 +24,9 @@ func UnpackUint32(data []byte, offset int) (uint32, int) {
 	return binary.BigEndian.Uint32(data[offset:]), offset + 4
 }
 
+// FIXME (Techassi): Add ability to follow pointers marked by upper 2 bits of an octet set to 1 followed by 14 bits of
+// offset (6 bits from first octet + one octet)
+
 // UnpackDomainName unwraps a domain name in a DNS question or in a RR header
 func UnpackDomainName(data []byte, offset int) (string, int) {
 	// If we immediation encounter a null byte, the name is root (.)
@@ -29,29 +34,37 @@ func UnpackDomainName(data []byte, offset int) (string, int) {
 		return ".", offset + 1
 	}
 
+	var offsetBeforePtr = 0
+	var followed bool
 	var buf []byte
+	var done bool
 
-	// Initialize the end of the label in bytes
-	end := offset + int(data[offset]) + 1
-	offset++
-
-	// Iterate over the bytes until we reach the null byte, which
-	// marks the root (.)
-	for {
-		if data[offset] == 0x00 {
-			buf = append(buf, '.')
-			offset++
-			break
-		}
-
-		if offset == end {
-			buf = append(buf, '.')
-			end += int(data[offset]) + 1
-			offset++
-		}
-
-		buf = append(buf, data[offset])
+	for !done {
+		b := int(data[offset])
 		offset++
+
+		// Check if we have a pointer (11000000 => 0xC0)
+		switch b & 0xC0 {
+		case 0x00:
+			if b == 0x00 {
+				done = true
+				break
+			}
+
+			buf = append(buf, data[offset:offset+b]...)
+			buf = append(buf, '.')
+			offset += b
+		case 0xC0:
+			if !followed {
+				offsetBeforePtr = offset + 1
+			}
+			offset = int(b^0xC0)<<8 | int(data[offset])
+			followed = true
+		}
+	}
+
+	if followed {
+		offset = offsetBeforePtr
 	}
 
 	return string(buf), offset
