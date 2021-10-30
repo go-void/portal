dbdba2b866f2ce9d9e140e27adddfd4a01e6ea2c
Techassi <git@techassi.dev>
2021-10-30 14:44:39 +0200

    Adjust resolver API

--- internal/resolver/resolver.go
+++ internal/resolver/resolver.go
@@ -1,18 +1,23 @@
 package resolver
 
 import (
-	"github.com/go-void/portal/internal/cache"
+	"errors"
+
 	"github.com/go-void/portal/internal/types/dns"
 	"github.com/go-void/portal/internal/types/rr"
+)
+
+var (
+	ErrNoAnswer = errors.New("no answer")
 )
 
 // Resolver describes a DNS resolver which can resolve
 // DNS queries (of unknown / external names) iteratively
 // or recursively
 type Resolver interface {
-	// Resolve resolves a query either iteratively,
-	// recursively or by forwarding it to a upstream
-	// DNS server
+	// Resolve resolves a query by first looking if
+	// there is a cached record and if not it will
+	// look up using remote DNS servers
 	Resolve(string, uint16, uint16) (rr.RR, error)
 
 	// ResolveQuestion is a convenience function which
@@ -20,10 +25,14 @@ type Resolver interface {
 	// individual parameters
 	ResolveQuestion(dns.Question) (rr.RR, error)
 
-	// Cache provides access to a cache instance to
-	// store answers from remote DNS servers for
-	// the given TTL
-	Cache() cache.Cache
+	// Lookup looks up a query either iteratively,
+	// recursively or by forwarding it to a upstream
+	// DNS server
+	Lookup(string, uint16, uint16) (rr.RR, error)
+
+	// Refresh refreshes a cached record by looking
+	// it up again
+	Refresh(string, uint16, uint16)
 }
 
 // TODO (Techassi): Figure this out
