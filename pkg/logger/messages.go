2a4d0fc6648b6a1860e739d729a873f8ec3eb143
Techassi <git@techassi.dev>
2022-01-13 20:54:42 +0100

    Add log messages

---
+++ pkg/logger/messages.go
@@ -0,0 +1,25 @@
+package logger
+
+// General log messages
+const (
+	ErrUnpackDNSHeader  = "failed to unpack DNS message header"
+	ErrUnpackDNSMessage = "failed to unpack DNS message"
+	ErrPackDNSMessage   = "failed to pack DNS message"
+	ErrAcceptMessage    = "did not accept DNS message"
+	ErrHandleRequest    = "failed to handle incoming DNS request"
+)
+
+// UDP related log messages
+const (
+	ErrUDPRead  = "failed to read UDP datagram"
+	ErrUDPWrite = "failed to write UDP datagram"
+)
+
+// TCP related log messages
+const (
+	ErrTCPAccept     = "failed to accept TCP conn"
+	ErrTCPClose      = "failed to close TCP conn"
+	ErrTCPRead       = "failed to read TCP packet"
+	ErrTCPWrite      = "failed to write TCP packet"
+	ErrTCPWriteClose = "failed to write packet or close TCP conn"
+)
