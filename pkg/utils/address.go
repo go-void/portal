7d0c41d42451a346cf8230f3b3bb1757f46348f4
Techassi <git@techassi.dev>
2022-01-15 17:47:06 +0100

    Adjust utils.DNSAddress func

--- pkg/utils/address.go
+++ pkg/utils/address.go
@@ -1,15 +1,10 @@
 package utils
 
 import (
-	"fmt"
 	"net"
 )
 
 // DNSAddress returns an address with the following format: <ip-address>:53
 func DNSAddress(ip net.IP) string {
-	if len(ip) == 16 {
-		return fmt.Sprintf("%s:%d", ip.String(), 53)
-	} else {
-		return fmt.Sprintf("[%s]:%d", ip.String(), 53)
-	}
+	return net.JoinHostPort(ip.String(), "53")
 }
